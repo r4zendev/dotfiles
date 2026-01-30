0d4a024b52b6f961367a7cba0f543efef531cd14
r4zendev <reactionnnnn@gmail.com>
2026-01-30 18:59:24 +0100

    typos config fix

--- stow/common/.config/nvim/after/lsp/typos_lsp.lua
+++ stow/common/.config/nvim/after/lsp/typos_lsp.lua
@@ -1,8 +1,6 @@
 return {
   cmd_env = { RUST_LOG = "error" },
   init_options = {
-    -- Equivalent to the typos `--config` cli argument.
-    config = "~/.config/nvim/_typos.toml",
     -- How typos are rendered in the editor, can be one of an Error, Warning, Info or Hint.
     diagnosticSeverity = "Hint",
   },
